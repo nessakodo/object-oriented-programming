9f5001016481bf83847ea1408d8f5742280bda7f
nessakodo <nessakodo@gmail.com>
2024-02-01 18:23:17 -0700

    adding week-4

---
+++ week-four/main.cpp
@@ -0,0 +1,11 @@
+#include "Canine.h"
+#include <iostream>
+
+int main()
+{
+    Canine aCanine = Canine("Nano", "Pomeranian", 1.72, 11, "Sable", 'M');
+
+    aCanine.getCanine();
+
+    return 0;
+}
